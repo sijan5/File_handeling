739ca704ee7e550a7568e0cf10334e50da5f099e
sijan5 <107232540+sijan5@users.noreply.github.com>
2023-01-15 00:13:20 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+Process process = new Process();
+process.StartInfo.FileName = "cmd.exe";
+process.StartInfo.Arguments = "/c mkdir sumitkando&&cd sumitkando&&dotnet new console&&dotnet restore";
+process.Start();
+process.WaitForExit();
+File.WriteAllText("sumitkando/program.cs","Console.Write(\"sumit is kalo kando\");");
+process.StartInfo.FileName = "cmd.exe";
+process.StartInfo.Arguments = "/ccd sumitkando&&dotnet run";
+process.Start();
+process.WaitForExit();
